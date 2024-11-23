e2098b5cb3fcd92c0e1fe2a89f45815728421fd0
Mac Osx <macosx@MBP-de-Mac.local>
2024-11-23 10:07:10 -0300

    first commit

---
+++ secretword/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { wordList } from './data/word';
+
+
+function App() {
+ 
+
+  return (
+   <div className="App">
+    <h2>Secret word</h2>
+   </div>  
+  )
+}
+
+export default App;
---
+++ secretword/src/data/word.jsx
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ["Motor", "Porta" , "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maça", "Pera", "Mamao", "Laranja"],
+    corpo: ["Braço", "Perna" , "Cerebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programaçao: ["", "", "", ""],
+    alimento: ["Ärroz", "Feijao", "Carne", "Leite", "Ovo"],
+};
